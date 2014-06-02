fe59959eddec7818adebe60c11eb7461524bd14a
XAPBob <john@dawnlink.ltd.uk>
2014-06-03 00:56:46 +0100

    Untested code: PRT/LRT influences should be correct (even if the JSON is made up) assuming HE growth is stored doubled Need to verify overpop planet "die off rate"
    Update tools.py:
    planet['hab'] is a tuple: (grav,temp,rad)
    clicks in each
    player['hab'] is normally a tuple of tuples: ((min_grav,max_grav,centre_grav),...)  However any subtuple may be replaced by the string "immune": ("Immune",(20,80,0),(30,70,0)) for instance.
    This allows for future "off centre" hab settings without affecting the basic calculation.  The RW will currently limit the centre_grav to bisecting between the min and max.  This code can accept offsets as per the original.

--- engine/pop_growth.py
+++ engine/pop_growth.py
@@ -3,16 +3,52 @@ Stars order of events
 19. Population grows/dies
 """
 
-# TODO: use hab values
-# TODO: add in crowding
-# TODO: deal with overcrowding
-
-from .tools import get_owner
+from .tools import get_owner, get_hab
 
 def run(universe, orders=None):
     for planet in universe['planets']:
         if 'population' in planet:
             player = get_owner(universe, planet)
-            pgr = (100 + player['pop_growth']) / 100.
-            planet['population'] = int(planet['population']) * pgr
+            planet['population'] = growth(planet, player)
     return universe
+
+def new_population(planet, player)
+    pgr = player['pop_growth']
+    hab = get_hab(planet, player)/100
+    if player['prt']="HE":
+        pgr=pgr*2
+    elif player['prt']="AR":
+        max_pop=planet['orbital']
+        if planet['orbital']="Fort":
+            max_pop=250000
+        elif planet['orbital']="Dock":
+            max_pop=500000
+        elif planet['orbital']="Station":
+            max_pop=1000000
+        elif planet['orbital']="Ultra":
+            max_pop=2000000
+        elif planet['orbital']="DeathStar":
+            max_pop=3000000
+    elif player['prt']="JoAT":
+        max_pop=1200000*hab
+    else:
+        max_pop=1000000*hab
+    
+    if player['obrm']:
+        max_pop=max_pop*1.1
+
+    growth=(100+pgr)/100 * hab
+
+    if hab < 0: #RED planets kill at 10% of their -ve value
+        return int(planet['population'] * (1-(hab/10))
+    elif planet['population'] <= (.25 * max_pop): # Uncrowded planet growth is unrestricted
+        return int(planet['population'] * growth)    
+    elif planet['population'] <= max_pop: # Crowding reduces growth
+        crowding=16/9*(1-planet['population']/max_pop)^2
+        return int(planet['population'] * growth * crowding)
+    elif planet['population'] >= 4*max_pop: # Massive overpop dies at 12%
+        return int(planet['population'] * (1-.12))
+    else: # Overpop death is linear up to 12%
+        over_pop=(planet['population']-max_pop)/(3*max_pop)
+        return int(planet['population'] * (1-((1-over_pop)*.12)) )
+        
--- engine/tools.py
+++ engine/tools.py
@@ -40,3 +40,33 @@ def calc_distance(a, b):
     Both objects should have ['x'] and ['y'] keys
     """
     return math.sqrt((a['x'] - b['x'])**2 + (a['y'] - b['y'])**2)
+
+def get_hab(planet, player):
+    """
+    Compute the hab value for a planet for a given player
+    """
+    planet_value_points=0
+    ideality=10000
+    red_value=0
+    
+    for i in range(0,3):
+        distance=abs(player['hab'][i][3] - planet['hab'][i])
+        if planet['hab'][i] > player['hab'][i][3]:
+            radius=player['hab'][i][2] - player['hab'][i][3]
+        else:
+            radius=player['hab'][i][3] - player['hab'][i][1]
+        if player['hab'][i] = "immune":
+            planet_value_points += 10000
+        elif planet['hab'][[i] < player['hab'][i][2] and planet['hab'][i] > player['hab'][i][1]:
+            excent=100-(int(100*distance/radius)
+            planet_value_point =+ excent^2
+            if distance *2 > radius: #Outer quarter of hab range gets a penalty
+                ideality=ideality*(3/2 - (distance/radius))
+        else: #Red on this variable
+            red_value+=max(-15,radius-distance)
+
+    if red_value != 0:
+        return red_value
+    planet_value_points = sqrt(planet_value_points/3)+0.9
+    planet_value_points = planet_value_points * int(ideality)/10000
+    return int(planet_value_points)
