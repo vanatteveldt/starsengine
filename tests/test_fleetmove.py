028d309c6d105d72ba3daddef82be2bea5b1bbdc
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-06-07 13:25:06 +0200

    fix fleetmove nosetest after return type refactor

--- tests/test_fleetmove.py
+++ tests/test_fleetmove.py
@@ -4,30 +4,31 @@ from engine import move_fleets
 from engine.tools import get_fleet
 
 def test_travel():
-    def assert_location(universe, fleet_id, x, y):
+    universe = {"fleets": [{"id": 1, "x": 0, "y": 0}]}
+
+    def assert_location(fleet_id, x, y):
         f = get_fleet(universe, fleet_id)
         assert_equal(f['x'], x)
         assert_equal(f['y'], y)
 
-    # move from 0,0 to 100,0 at warp 5
-    universe = {"fleets": [{"id": 1, "x": 0, "y": 0}]}
+    # move from to 100,0 at warp 5
     order = {"fleetorders": [
         {"fleet": 1, "x": 100, "y": 0, "warp": 5}
     ]}
 
-    result = move_fleets.run(universe, [order])
-    assert_location(result, 1, 25, 0)
+    move_fleets.run(universe, [order])
+    assert_location(1, 25, 0)
 
     # Don't overshoot
     order = {"fleetorders": [
         {"fleet": 1, "x": 25, "y": 25, "warp": 9}
     ]}
-    result = move_fleets.run(universe, [order])
-    assert_location(result, 1, 25, 25)
+    move_fleets.run(universe, [order])
+    assert_location(1, 25, 25)
 
     # moving 25.7 ly at warp 5 gets there in one turn
     order = {"fleetorders": [
         {"fleet": 1, "x": 25, "y": 5, "warp": 5}
     ]}
-    result = move_fleets.run(universe, [order])
-    assert_location(result, 1, 25, 5)
+    move_fleets.run(universe, [order])
+    assert_location(1, 25, 5)
